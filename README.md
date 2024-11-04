9062cb5c2149143de11ff4886c658e6fd5a4bedc
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-11-04 11:01:27 +0800

    chore: remove unused Microsoft API environment variables and update README for setup instructions

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # HKUST Information Push
 
 A set of tools to push information to HKUST students.
+
+## Setting Up Microsoft Email Summarization
+
+We have to use the Microsoft Graph API to access the emails. To do this, we need to set up an application in Azure AD.
+
+1. Go to the [Azure Portal](https://portal.azure.com/), and go to "App registrations" under "Entra ID".
+2. Click "New registration".
+3. Fill in the details.
+    - Multiple accounts supported: `Accounts in any organizational directory (Any Microsoft Entra ID tenant - Multitenant) and personal Microsoft accounts (e.g. Skype, Xbox)`
+    - Redirect URI: `https://login.microsoftonline.com/common/oauth2/nativeclient` (Mobile)
+4. After creating the application, go to "Certificates & secrets" and create a new client secret.
+5. Copy the client secret and the application (client) ID.
+6. Go to "API permissions" and add the following permissions:
+    - `Mail.Read`
+    - `Mail.Read.Shared`
+    - `User.Read`
--- lib/microsoft_tokens.py
+++ lib/microsoft_tokens.py
@@ -1,5 +1,4 @@
 import json
-import webbrowser
 
 import requests
 from rich.console import Console
@@ -8,47 +7,22 @@ import lib.env as env
 
 console = Console()
 
-# https://learn.microsoft.com/en-us/graph/permissions-reference
 
-
-def run_browser_authorization():
-    url = f"https://login.microsoftonline.com/{env.MICROSOFT_TENANT_ID}/oauth2/v2.0/authorize"
-
-    querystring = {
-        "code_challenge": env.MICROSOFT_CODE_CHALLENGE,
-        "code_challenge_method": "S256",
-        "prompt": "login",
-        "redirect_uri": "https://jwt.ms",
-        "response_mode": "query",
-        "client_id": env.MICROSOFT_CLIENT_ID,
-        "response_type": "code",
-        "state": "00000",  # random state
-        "scope": "profile openid offline_access email https://graph.microsoft.com/user.read https://graph.microsoft.com/mail.read",
-    }
-
-    webbrowser.open(url + "?" + "&".join([f"{k}={v}" for k, v in querystring.items()]))
-
-
-def get_private_graph_token(full=False):
-    url = (
-        f"https://login.microsoftonline.com/{env.MICROSOFT_TENANT_ID}/oauth2/v2.0/token"
-    )
+def get_private_graph_token():
+    url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
 
     payload = {
-        "grant_type": (
-            "refresh_token" if env.MICROSOFT_TENANT_ID else "authorization_code"
-        ),
-        "client_id": env.MICROSOFT_CLIENT_ID,
-        "scope": "profile openid offline_access email https://graph.microsoft.com/user.read https://graph.microsoft.com/mail.read",
-        "code": env.MICROSOFT_CODE,
-        "redirect_uri": "https://jwt.ms",
-        "code_verifier": env.MICROSOFT_CODE_VERIFIER,
-        "client_secret": env.MICROSOFT_CLIENT_SECRET,
+        "grant_type": "refresh_token",
         "refresh_token": env.MICROSOFT_REFRESH_TOKEN,
+        "CLIENT_ID": env.MICROSOFT_CLIENT_ID,
+        "REDIRECT_URL": "https://jwt.ms",
+        "CLIENT_SECRET": env.MICROSOFT_CLIENT_SECRET,
     }
 
     response = requests.post(url, data=payload)
 
-    return (
-        json.loads(response.text) if full else json.loads(response.text)["access_token"]
-    )
+    if response.status_code != 200:
+        console.print(f"Error: {response.text}")
+        return None
+
+    return json.loads(response.text)["access_token"]
