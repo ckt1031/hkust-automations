8f2fea3c313cd854f141cdc5a1378e8af6456fe9
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-12-21 00:30:59 +0800

    feat(webhook.py): add cooldown management with shelve storage for better state persistence fix(webhook.py): refactor cooldown logic to use `get_cooldown_status` and `set_cooldown_status` functions

--- discord/webhook.py
+++ discord/webhook.py
@@ -1,11 +1,30 @@
+import shelve
 from datetime import datetime, timedelta
 from time import sleep
 
 import requests
 from loguru import logger
 
-COOLDOWN_REQUIRED = False
-REMAINING_EXPIRY = datetime.now()
+SHELVE_PATH = "./tmp/discord"
+
+
+def get_cooldown_status():
+    with shelve.open(SHELVE_PATH) as db:
+        expiry = db.get("remaining_expiry", datetime.now().isoformat())
+        expiry = datetime.fromisoformat(expiry)
+
+        data = {
+            "cooldown_required": db.get("cooldown_required", False),
+            "remaining_expiry": expiry,
+        }
+
+    return data
+
+
+def set_cooldown_status(cooldown_required: bool, remaining_expiry: datetime):
+    with shelve.open(SHELVE_PATH) as db:
+        db["cooldown_required"] = cooldown_required
+        db["remaining_expiry"] = remaining_expiry.isoformat()
 
 
 def send_discord_webhook(
@@ -14,10 +33,15 @@ def send_discord_webhook(
     embed: dict | None = None,
     username="School",
 ):
-    global COOLDOWN_REQUIRED, REMAINING_EXPIRY
+    cooldown_status = get_cooldown_status()
 
-    if COOLDOWN_REQUIRED and REMAINING_EXPIRY > datetime.now():
-        seconds_left = (REMAINING_EXPIRY - datetime.now()).total_seconds()
+    if (
+        cooldown_status["cooldown_required"]
+        and cooldown_status["remaining_expiry"] > datetime.now()
+    ):
+        seconds_left = (
+            cooldown_status["remaining_expiry"] - datetime.now()
+        ).total_seconds()
 
         logger.debug(
             f"Discord webhook rate limit reached, sleeping for {seconds_left}s"
@@ -25,7 +49,7 @@ def send_discord_webhook(
 
         sleep(seconds_left)
 
-        COOLDOWN_REQUIRED = False
+        set_cooldown_status(False, datetime.now())
 
     data = {"content": message, "username": username}
 
@@ -45,8 +69,8 @@ def send_discord_webhook(
     if remaining is None or reset_after is None:
         return
 
-    REMAINING = int(remaining)
+    remaining = int(remaining)
 
-    if REMAINING == 1:
-        COOLDOWN_REQUIRED = True
-        REMAINING_EXPIRY = datetime.now() + timedelta(seconds=float(reset_after))
+    if remaining == 1:
+        expiry = datetime.now() + timedelta(seconds=float(reset_after))
+        set_cooldown_status(True, expiry)
