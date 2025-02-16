d6944c0111fcaa549130f07dda1c45c7a8fd1116
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-02-16 11:50:55 +0800

    revert: USThing token is generated in unexpected move

--- usthing/api.py
+++ usthing/api.py
@@ -1,34 +1,43 @@
+from functools import cache
+
 import requests
 
 from lib.microsoft_tokens import get_me_info, get_usthing_private_graph_token
 
-TOKEN = get_usthing_private_graph_token()
 HEADERS = {
     "Accept": "application/json",
     "User-Agent": "USThing/113 CFNetwork/1568.100.1 Darwin/24.0.0",
     "Accept-Language": "en-GB,en;q=0.9",
-    "Authorization": f"Bearer {TOKEN}",
 }
 MS_API_BASE_URL = "https://ms.api.usthing.xyz/v2"
 
 
 def get_username():
-    me_info = get_me_info(TOKEN)
+    token = get_usthing_private_graph_token()
+    me_info = get_me_info(token)
 
     return me_info["mail"].split("@")[0]
 
 
+@cache
 def get_course_grades():
     username = get_username()
+    token = get_usthing_private_graph_token()
     url = f"{MS_API_BASE_URL}/sis/stdt_courses?userName={username}"
-    res = requests.get(url, headers=HEADERS, timeout=10)
+    res = requests.get(
+        url, headers={"Authorization": f"Bearer {token}", **HEADERS}, timeout=10
+    )
     res.raise_for_status()
     return res.json()
 
 
+@cache
 def get_class_enrollments():
     username = get_username()
+    token = get_usthing_private_graph_token()
     url = f"{MS_API_BASE_URL}/sis/stdt_class_enrl?userName={username}"
-    res = requests.get(url, headers=HEADERS, timeout=10)
+    res = requests.get(
+        url, headers={"Authorization": f"Bearer {token}", **HEADERS}, timeout=10
+    )
     res.raise_for_status()
     return res.json()
